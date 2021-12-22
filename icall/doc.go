51d57019547a3341623bab39c5d964590420fa27
visualfc <visualfc@gmail.com>
2021-12-22 21:43:18 +0800

    update icall

--- icall/doc.go
+++ icall/doc.go
@@ -1,9 +1,9 @@
 package icall
 
-//go:generate icall_gen -o ./icall10/icall.go -size 1024
-//go:generate icall_gen -o ./icall11/icall.go -size 2048
-//go:generate icall_gen -o ./icall12/icall.go -size 4096
-//go:generate icall_gen -o ./icall13/icall.go -size 8192
-//go:generate icall_gen -o ./icall14/icall.go -size 16384
-//go:generate icall_gen -o ./icall15/icall.go -size 32768
-//go:generate icall_gen -o ./icall16/icall.go -size 65536
+//go:generate icall_gen -o ./icall1024/icall.go -size 1024
+//go:generate icall_gen -o ./icall2048/icall.go -size 2048
+//go:generate icall_gen -o ./icall4096/icall.go -size 4096
+//go:generate icall_gen -o ./icall8192/icall.go -size 8192
+//go:generate icall_gen -o ./icall16384/icall.go -size 16384
+//go:generate icall_gen -o ./icall32768/icall.go -size 32768
+//go:generate icall_gen -o ./icall65536/icall.go -size 65536
