c9d8b8d8cf21226f096982e2f1ca243e87e04267
visualfc <visualfc@gmail.com>
2022-02-06 19:14:19 +0800

    regabi: add go1.18 arm64 support

--- icall_regabi_arm64.s
+++ icall_regabi_arm64.s
@@ -1,5 +1,5 @@
-//go:build go1.17 && goexperiment.regabireflect
-// +build go1.17,goexperiment.regabireflect
+//go:build go1.18 && goexperiment.regabireflect
+// +build go1.18,goexperiment.regabireflect
 
 // Copyright 2012 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
@@ -7,7 +7,6 @@
 
 #include "textflag.h"
 #include "funcdata.h"
-#include "go_asm.h"
 
 // The frames of each of the two functions below contain two locals, at offsets
 // that are known to the runtime.
@@ -20,89 +19,35 @@
 // The second local is an abi.RegArgs value whose offset is also known to the
 // runtime, so that a stack map for it can be constructed, since it contains
 // pointers visible to the GC.
-#define LOCAL_RETVALID 32
-#define LOCAL_REGARGS 40
+#define LOCAL_RETVALID 40
+#define LOCAL_REGARGS 48
 
-// spillArgs stores return values from registers to a *internal/abi.RegArgs in R12.
-TEXT ·spillArgs(SB),NOSPLIT,$0-0
-	MOVQ AX, 0(R12)
-	MOVQ BX, 8(R12)
-	MOVQ CX, 16(R12)
-	MOVQ DI, 24(R12)
-	MOVQ SI, 32(R12)
-	MOVQ R8, 40(R12)
-	MOVQ R9, 48(R12)
-	MOVQ R10, 56(R12)
-	MOVQ R11, 64(R12)
-	MOVQ X0, 72(R12)
-	MOVQ X1, 80(R12)
-	MOVQ X2, 88(R12)
-	MOVQ X3, 96(R12)
-	MOVQ X4, 104(R12)
-	MOVQ X5, 112(R12)
-	MOVQ X6, 120(R12)
-	MOVQ X7, 128(R12)
-	MOVQ X8, 136(R12)
-	MOVQ X9, 144(R12)
-	MOVQ X10, 152(R12)
-	MOVQ X11, 160(R12)
-	MOVQ X12, 168(R12)
-	MOVQ X13, 176(R12)
-	MOVQ X14, 184(R12)
-	RET
-
-// unspillArgs loads args into registers from a *internal/abi.RegArgs in R12.
-TEXT ·unspillArgs(SB),NOSPLIT,$0-0
-	MOVQ 0(R12), AX
-	MOVQ 8(R12), BX
-	MOVQ 16(R12), CX
-	MOVQ 24(R12), DI
-	MOVQ 32(R12), SI
-	MOVQ 40(R12), R8
-	MOVQ 48(R12), R9
-	MOVQ 56(R12), R10
-	MOVQ 64(R12), R11
-	MOVQ 72(R12), X0
-	MOVQ 80(R12), X1
-	MOVQ 88(R12), X2
-	MOVQ 96(R12), X3
-	MOVQ 104(R12), X4
-	MOVQ 112(R12), X5
-	MOVQ 120(R12), X6
-	MOVQ 128(R12), X7
-	MOVQ 136(R12), X8
-	MOVQ 144(R12), X9
-	MOVQ 152(R12), X10
-	MOVQ 160(R12), X11
-	MOVQ 168(R12), X12
-	MOVQ 176(R12), X13
-	MOVQ 184(R12), X14
-	RET
+// The frame size of the functions below is
+// 32 (args of callReflect) + 8 (bool + padding) + 392 (abi.RegArgs) = 432.
 
 // makeFuncStub is the code half of the function returned by MakeFunc.
 // See the comment on the declaration of makeFuncStub in makefunc.go
 // for more details.
-// No arg size here; runtime pulls arg map out of the func value.
-// This frame contains two locals. See the comment above LOCAL_RETVALID.
+// No arg size here, runtime pulls arg map out of the func value.
 #define MAKE_FUNC_FN(NAME,INDEX)		\
-TEXT NAME(SB),(NOSPLIT|WRAPPER),$312		\
+TEXT NAME(SB),(NOSPLIT|WRAPPER),$432		\
 	NO_LOCAL_POINTERS		\
-	LEAQ	LOCAL_REGARGS(SP), R12		\
-	CALL	·spillArgs(SB)		\
-	MOVQ	24(SP), DX		\
-	MOVQ	DX, 0(SP)		\
-	LEAQ	argframe+0(FP), CX		\
-	MOVQ	CX, 8(SP)		\
-	MOVB	$0, LOCAL_RETVALID(SP)		\
-	LEAQ	LOCAL_RETVALID(SP), AX		\
-	MOVQ	AX, 16(SP)		\
-	LEAQ	LOCAL_REGARGS(SP), AX		\
-	MOVQ	AX, 24(SP)		\
-	MOVQ	$INDEX, AX		\
-	MOVQ	AX, 32(SP)		\
-	CALL	·i_amd64(SB)		\
-	LEAQ	LOCAL_REGARGS(SP), R12		\
-	CALL	·unspillArgs(SB)		\
+	ADD	$LOCAL_REGARGS, RSP, R20		\
+	CALL	runtime·spillArgs(SB)		\
+	MOVD	32(RSP), R26		\
+	MOVD	R26, 8(RSP)		\
+	MOVD	$argframe+0(FP), R3		\
+	MOVD	R3, 16(RSP)		\
+	MOVB	$0, LOCAL_RETVALID(RSP)		\
+	ADD	$LOCAL_RETVALID, RSP, R3		\
+	MOVD	R3, 24(RSP)		\
+	ADD	$LOCAL_REGARGS, RSP, R3		\
+	MOVD	R3, 32(RSP)		\
+	MOVD	$INDEX, R3		\
+	MOVD	R3, 40(RSP)		\
+	CALL	·i_arm64(SB)		\
+	ADD	$LOCAL_REGARGS, RSP, R20		\
+	CALL	runtime·unspillArgs(SB)		\
 	RET
 
 MAKE_FUNC_FN(·f0,0)
