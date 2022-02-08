bcd6888d996f82abff1f9e84c8360a0bc70726af
visualfc <visualfc@gmail.com>
2022-02-08 09:05:47 +0800

    update cmd/icall_gen

--- cmd/icall_gen/icall_regabi.go
+++ cmd/icall_gen/icall_regabi.go
@@ -41,6 +41,7 @@ var (
 
 	asm_amd64_file := filepath.Join(dir, strings.Replace(f, ".go", "_regabi_amd64.s", 1))
 	asm_arm64_file := filepath.Join(dir, strings.Replace(f, ".go", "_regabi_arm64.s", 1))
+	asm_ppc64x_file := filepath.Join(dir, strings.Replace(f, ".go", "_regabi_ppc64x.s", 1))
 	fnWrite := func(filename string, tmpl string, size int) error {
 		var buf bytes.Buffer
 		buf.WriteString(tmpl)
@@ -54,6 +55,10 @@ var (
 		return err
 	}
 	err = fnWrite(asm_arm64_file, regabi_arm64, size)
+	if err != nil {
+		return err
+	}
+	err = fnWrite(asm_ppc64x_file, regabi_ppc64x, size)
 	if err != nil {
 		return err
 	}
@@ -87,12 +92,7 @@ var infos []*reflectx.MethodInfo
 var funcs []reflect.Value
 var fnptr []unsafe.Pointer
 
-func i_amd64(c unsafe.Pointer, frame unsafe.Pointer, retValid *bool, r unsafe.Pointer, index int) {
-	moveMakeFuncArgPtrs(fnptr[index], r)
-	callReflect(fnptr[index], unsafe.Pointer(uintptr(frame)+8), retValid, r)
-}
-
-func i_arm64(c unsafe.Pointer, frame unsafe.Pointer, retValid *bool, r unsafe.Pointer, index int) {
+func i_x(c unsafe.Pointer, frame unsafe.Pointer, retValid *bool, r unsafe.Pointer, index int) {
 	moveMakeFuncArgPtrs(fnptr[index], r)
 	callReflect(fnptr[index], frame, retValid, r)
 }
@@ -251,7 +251,7 @@ TEXT NAME(SB),(NOSPLIT|WRAPPER),$312		\
 	CALL	·spillArgs(SB)		\
 	MOVQ	24(SP), DX		\
 	MOVQ	DX, 0(SP)		\
-	LEAQ	argframe+0(FP), CX		\
+	LEAQ	argframe+8(FP), CX		\
 	MOVQ	CX, 8(SP)		\
 	MOVB	$0, LOCAL_RETVALID(SP)		\
 	LEAQ	LOCAL_RETVALID(SP), AX		\
@@ -260,7 +260,7 @@ TEXT NAME(SB),(NOSPLIT|WRAPPER),$312		\
 	MOVQ	AX, 24(SP)		\
 	MOVQ	$INDEX, AX		\
 	MOVQ	AX, 32(SP)		\
-	CALL	·i_amd64(SB)		\
+	CALL	·i_x(SB)		\
 	LEAQ	LOCAL_REGARGS(SP), R12		\
 	CALL	·unspillArgs(SB)		\
 	RET
@@ -314,8 +314,66 @@ TEXT NAME(SB),(NOSPLIT|WRAPPER),$432		\
 	MOVD	R3, 32(RSP)		\
 	MOVD	$INDEX, R3		\
 	MOVD	R3, 40(RSP)		\
-	CALL	·i_arm64(SB)		\
+	CALL	·i_x(SB)		\
 	ADD	$LOCAL_REGARGS, RSP, R20		\
+	CALL	runtime·unspillArgs(SB)		\
+	RET
+
+`
+
+var regabi_ppc64x = `//go:build go1.18 && goexperiment.regabireflect && (ppc64 || ppc64le)
+// +build go1.18
+// +build goexperiment.regabireflect
+// +build ppc64 ppc64le
+
+// Copyright 2012 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include "textflag.h"
+#include "funcdata.h"
+#include "asm_ppc64x.h"
+
+// The frames of each of the two functions below contain two locals, at offsets
+// that are known to the runtime.
+//
+// The first local is a bool called retValid with a whole pointer-word reserved
+// for it on the stack. The purpose of this word is so that the runtime knows
+// whether the stack-allocated return space contains valid values for stack
+// scanning.
+//
+// The second local is an abi.RegArgs value whose offset is also known to the
+// runtime, so that a stack map for it can be constructed, since it contains
+// pointers visible to the GC.
+
+#define LOCAL_RETVALID 32+FIXED_FRAME
+#define LOCAL_REGARGS 40+FIXED_FRAME
+
+// The frame size of the functions below is
+// 32 (args of callReflect) + 8 (bool + padding) + 296 (abi.RegArgs) = 336.
+
+// makeFuncStub is the code half of the function returned by MakeFunc.
+// See the comment on the declaration of makeFuncStub in makefunc.go
+// for more details.
+// No arg size here, runtime pulls arg map out of the func value.
+#define MAKE_FUNC_FN(NAME,INDEX)		\
+TEXT NAME(SB),(NOSPLIT|WRAPPER),$336		\
+	NO_LOCAL_POINTERS		\
+	ADD	$LOCAL_REGARGS, R1, R20		\
+	CALL	runtime·spillArgs(SB)		\
+	MOVD	FIXED_FRAME+32(R1), R11			\
+	MOVD	R11, FIXED_FRAME+0(R1)		\
+	MOVD	$argframe+0(FP), R3		\
+	MOVD	R3, FIXED_FRAME+8(R1)		\
+	ADD	$LOCAL_RETVALID, R1, R3		\
+	MOVB	R0, (R3)		\
+	MOVD	R3, FIXED_FRAME+16(R1)			\
+	ADD     $LOCAL_REGARGS, R1, R3		\
+	MOVD	R3, FIXED_FRAME+24(R1)		\
+	MOVD	$INDEX, R3		\
+	MOVD	R3, FIXED_FRAME+32(R1)		\
+	BL	·i_x(SB)		\
+	ADD	$LOCAL_REGARGS, R1, R20		\
 	CALL	runtime·unspillArgs(SB)		\
 	RET
 
