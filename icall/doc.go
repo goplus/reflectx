0a60ed86745fd0ea1abfb51c92d83c38123a2737
visualfc <visualfc@gmail.com>
2025-10-30 21:07:35 +0800

    icall: add 512, remove 20480/40960/81920

--- icall/doc.go
+++ icall/doc.go
@@ -1,10 +1,8 @@
 package icall
 
+//go:generate icall_gen -o ./icall512/icall.go -size 512
 //go:generate icall_gen -o ./icall1024/icall.go -size 1024
 //go:generate icall_gen -o ./icall2048/icall.go -size 2048
 //go:generate icall_gen -o ./icall4096/icall.go -size 4096
 //go:generate icall_gen -o ./icall8192/icall.go -size 8192
 //go:generate icall_gen -o ./icall10240/icall.go -size 10240
-//go:generate icall_gen -o ./icall20480/icall.go -size 20480
-//go:generate icall_gen -o ./icall40960/icall.go -size 40960
-//go:generate icall_gen -o ./icall81920/icall.go -size 81920
